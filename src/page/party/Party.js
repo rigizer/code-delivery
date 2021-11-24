b5d47315064ca2719f67f6d6e229a12ecda2182f
Oven <kyo000619@naver.com>
2021-11-24 15:38:12 +0900

    input 검색어 박스 추가

--- src/page/party/Party.js
+++ src/page/party/Party.js
@@ -6,26 +6,27 @@ const Party = () => {
         <div class="party">
             <div class="party-main">
                 <div class="party-main__box party-main__box-option">
-                    <select name='' class="community-review__sort-item-select">
+                    <select name='' class="party-main__box-option-select">
                         <option value='all' selected>카테고리 선택</option>
                         <option value='1'>군단장 레이드</option>
                         <option value='2'>어비스 레이드</option>
                         <option value='3'>어비스 던전</option>
                         <option value='4'>집 가고싶다</option>
                     </select>
-                    <select name='' class="community-review__sort-item-select">
+                    <select name='' class="party-main__box-option-select">
                         <option value='all' selected>컨텐츠 선택</option>
                         <option value='1'>부활한 마수의 심장</option>
                         <option value='2'>목마른 쾌락의 정원</option>
                         <option value='3'>한밤중의 서커스</option>
                         <option value='4'>몽환의 아스텔지어</option>
                     </select>
-                    <select name='' class="community-review__sort-item-select">
+                    <select name='' class="party-main__box-option-select">
                         <option value='all' selected>난이도 선택</option>
                         <option value='1'>노말</option>
                         <option value='2'>하드</option>
                         <option value='3'>헬</option>
                     </select>
+                    <input type="text" name="search" class="party-main__box-option-search" placeholder="검색어를 입력해주세요." />
                 </div>
                 <div class="party-main__box party-main__box-character">캐릭터</div>
                 <div class="party-main__box party-main__box-list">
